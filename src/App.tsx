8bd9e327baf501cdb26ee76ce8d3cf4eda5fd278
sanil011 <sanilp191@gmail.com>
2026-05-13 20:59:53 +0530

    feat: complete the functionality

---
+++ src/App.tsx
@@ -0,0 +1,226 @@
+
+import { useEffect, useRef, useState } from 'react'
+import useSound from 'use-sound';
+import { cn } from './util/helper';
+import './App.css'
+
+const TARGET_TEXT = `Technology has changed the way we live work and communicate with each other.
+ Every day millions of people use computers and smartphones to connect with friends family and colleagues around the world.
+  The internet has made information available to anyone with a connection.`
+
+
+const VALID_KEYS = /^[a-zA-Z ]$|^\.$/ // letters, space, period
+
+
+function App() {
+  const divRef = useRef(null);
+  const allWords = useRef([]);
+
+  const words = TARGET_TEXT.split("");
+
+  const [timer, setTimer] = useState(50);
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [cursorPos, setCursorPos] = useState({ left: 0, top: 0, width: 0 })
+  const [typeCount, setTypeCount] = useState(0);
+  const [typedChars, setTypedChars] = useState<string[]>([]);
+  const [grossWPM, setGrossWPM] = useState(0);
+  const [netWPM, setNetWPM] = useState(0);
+
+  const [accuracy, setAccuracy] = useState(0);
+  const typedCharsRef = useRef<string[]>([]);
+
+  const [playSpace] = useSound('/space.mp3');
+  const [playWrong] = useSound('/wrong-type.mp3');
+  const [playCorrect] = useSound('/correct-type.mp3');
+  const timerRef = useRef(null);
+
+  const timeCompleted = () => {
+    const chars = typedCharsRef.current;
+
+    const correctCount = allWords.current.filter(
+      (el) => el?.classList.contains('text-primary')
+    ).length;
+
+    const uncorrectedErrors = allWords.current.filter(
+      (el) => el?.classList.contains('text-error')
+    ).length
+
+
+    const finalAccuracy = Math.round((correctCount / chars.length) * 100) || 0;
+
+    const gross = Math.round((chars.length / 5) / 1);
+    const netWPM = Math.round(gross - (uncorrectedErrors / 1))
+
+    setGrossWPM(gross);
+    setNetWPM(netWPM);
+    setAccuracy(finalAccuracy);
+
+    setIsCompleted(true);
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }
+
+  const handleClick = (e) => {
+    if (isCompleted) return;
+    if (!timerRef.current) {
+      timerRef.current = setInterval(() => {
+        setTimer((prev) => {
+          if (prev === 0) {
+            timeCompleted();
+            return 0
+          }
+          return prev - 1;
+        })
+      }, 1000)
+    }
+    if (e.code == 'Backspace') {
+      setTypedChars((prev) => {
+        const updated = [...prev, e.key];
+        typedCharsRef.current = updated;
+        return updated;
+      });
+      setTypeCount((prev) => prev - 1);
+      playSpace();
+      return;
+    }
+
+    if (!VALID_KEYS.test(e.key)) return
+    setTypedChars((prev) => {
+      const updated = [...prev, e.key];
+      typedCharsRef.current = updated;
+      return updated;
+    });
+    const elem = allWords.current[typeCount];
+    const keyWord = e.key;
+
+    setTypeCount((prev) => prev + 1);
+
+    if (keyWord == elem?.innerText) {
+      elem.classList.remove('text-error');
+      elem.classList.add('text-primary');
+      if (e.code === 'Space') {
+        playSpace();
+      } else {
+        playCorrect();
+      }
+
+    } else {
+      elem.classList.remove('text-primary');
+      elem.classList.add('text-error');
+      playWrong();
+    }
+
+  }
+
+  const handleTryAgain = () => {
+    setIsCompleted((prev) => !prev);
+    setTimer(50);
+    setTypeCount(0);
+    setTypedChars([]);
+  }
+  useEffect(() => {
+    const el = allWords.current[typeCount];
+
+    if (!el) return;
+
+    const left = el.offsetLeft;
+    const top = el.offsetTop;
+    const height = el.offsetHeight;
+    const width = el.offsetWidth;
+
+    setCursorPos({
+      left,
+      top: top + height,
+      width,
+    });
+  }, [typeCount]);
+
+
+  useEffect(() => {
+    if (!isCompleted) {
+      divRef.current.focus();
+    }
+  }, [isCompleted])
+
+
+
+  return (
+    <div
+      ref={divRef}
+      tabIndex={0}
+      onKeyUp={handleClick}
+      className='p-4 h-screen w-screen outline-none bg-[#1A1A1E] text-[#c1c2c6]'>
+
+      <h1 className='text-red-600 text-center text-2xl'>Practice Mode</h1>
+
+      <div className='w-1/2 mx-auto'>
+
+        {!isCompleted ?
+          <div
+            className='relative text-2xl leading-[40px]' style={{
+              letterSpacing: "2px"
+            }}>
+            <p>{timer}</p>
+            <div
+              className="absolute w-3 h-[2px]  bg-white"
+              style={{
+                top: `${cursorPos.top}px`,
+                left: `${cursorPos.left}px`,
+                width: `${cursorPos.width}px`,
+                animation: 'pulse 1s infinite'
+              }}
+            ></div>
+
+            {words?.map((word, index) => {
+              return (
+                <span
+                  ref={(i) => {
+                    if (i) allWords.current[index] = i;
+                  }}
+                  key={index}
+                  className={cn('opacity-70 transition-all', word == ' ' && "border-r w-10 border-transparent")}
+                  id={`letter-${index}`}>
+                  {word}
+                </span>
+              )
+            })}
+          </div> : <div>
+            <div>
+              <h1 className='text-lg font-bold mb-4'>Results</h1>
+
+              <div className='flex items-center gap-4 justify-between'>
+                <div>
+                  <h1 className=''>Gross WPM</h1>
+                  <h1 className='text-5xl font-bold'>{grossWPM}</h1>
+                </div>
+                <div>
+                  <h1>Net WPM</h1>
+                  <h1 className='text-5xl font-bold'>{netWPM}</h1>
+                </div>
+                <div>
+                  <h1>Accuracy</h1>
+                  <h1 className='text-5xl font-bold'>{accuracy}%</h1>
+                </div>
+                <div>
+                  <h1>Keystrokes</h1>
+                  <h1 className='text-5xl font-bold'>{typedChars.length}</h1>
+                </div>
+              </div>
+
+              <div className='flex items-center mt-4'>
+                <button
+                  onClick={handleTryAgain}
+                  className='mx-auto hover:bg-[#c1c2c6] hover:text-[#1A1A1E] cursor-pointer transition-all  p-1 text-xs rounded-md'>
+                  Try again
+                </button>
+              </div>
+            </div>
+
+
+          </div>}
+      </div>
+    </div>
+  )
+}
+
+export default App
